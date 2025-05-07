56ac9c6c7e8865880fe9031c86cc9c5aefd9acb6
agil ardian <135789717+AgilArdianP@users.noreply.github.com>
2025-05-07 16:07:20 +0700

    Update tarifRoutes.js

--- routes/tarifRoutes.js
+++ routes/tarifRoutes.js
@@ -5,9 +5,9 @@ const tarifController = require("../controllers/settingController");
 const { verifyToken } = require("../middlewares/authMiddleware");
 
 // Pastikan setiap request diverifikasi token-nya jika perlu
-router.get("/", verifyToken, tarifController.getTarif);
-router.post("/", verifyToken, tarifController.createTarif);
-router.put("/:id", verifyToken, tarifController.updateTarif);
-router.delete("/:id", verifyToken, tarifController.deleteTarif);
+router.get("/", verifyToken, settingController.getTarif);
+router.post("/", verifyToken, settingController.createTarif);
+router.put("/:id", verifyToken, settingController.updateTarif);
+router.delete("/:id", verifyToken, settingController.deleteTarif);
 
 module.exports = router;
